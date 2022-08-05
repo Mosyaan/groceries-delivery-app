e74bc6530798817b00d1a5b43541cdf946925c27
m0sya23 <86980860+m0sya23@users.noreply.github.com>
2022-08-05 16:56:44 +0300

    cart & card

---
+++ cart.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+/**
+ * <div class="card">
+ *                 <img src="assets/img/red_pepper.png" alt="">
+ *                 <div class="info">
+ *                     <p>Bell Pepper Red</p>
+ *                     <p>1kg, 6$</p>
+ *                 </div>
+ *                 <div class="amount">
+ *                     <button class="minus"></button>
+ *                     <p>1</p>
+ *                     <button class="plus"></button>
+ *                 </div>
+ *             </div>
+ */
